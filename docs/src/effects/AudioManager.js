01a9a451518470171f3a971c6c5efb08ac2a68d6
Marcos Íñigo <amargopastor@My-MacBook-Pro.local>
2021-12-30 12:00:24 +0100

    first commit

--- docs/src/effects/AudioManager.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.myAudioManager = exports.AudioManager = void 0;
-class AudioManager {
-}
-exports.AudioManager = AudioManager;
-exports.myAudioManager = new AudioManager();
--- docs/src/utils/angleToRad.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.angleToRad = void 0;
-const angleToRad = (angle) => (angle * Math.PI) / 180;
-exports.angleToRad = angleToRad;
